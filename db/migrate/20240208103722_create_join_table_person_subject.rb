7d22b202b84a82141bb4fe97f56d76b69cbd8903
menoudyann <Yann.MENOUD@cpnv.ch>
2024-02-22 09:02:23 +0100

    add column class to the table people_has_subjects

--- db/migrate/20240208103722_create_join_table_person_subject.rb
+++ db/migrate/20240208103722_create_join_table_person_subject.rb
@@ -3,6 +3,7 @@ class CreateJoinTablePersonSubject < ActiveRecord::Migration[7.1]
     create_table :people_has_subjects do |t|
       t.belongs_to :person, foreign_key: true
       t.belongs_to :subject, foreign_key: true
+      t.string :classe, null: false
     end
   end
 end
